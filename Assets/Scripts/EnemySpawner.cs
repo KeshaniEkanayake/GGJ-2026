439e1608a1404f49e048f999e9917807c009fd6a
Sadia Eva <evasadia375@gmail.com>
2026-02-01 10:39:29 -0400

    Progress on the EnemySpawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+    public float spawnInterval = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(spawnEnemy(spawnInterval, enemyPrefab));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-10f, 10f), 0, Random.Range(-10f, 10f)), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
     }
 }
